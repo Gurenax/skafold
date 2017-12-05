fdc9c326957318ae2c952466e364e8dd59815d25
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-12-05 23:41:56 +1100

    Cleanup binary

--- bin/skafold.js
+++ bin/skafold.js
@@ -1,8 +1,6 @@
 #! /usr/bin/env node
-// let shell = require('shelljs')
 const { skafold } = require('../index.js')
 console.log('Running Skafold...')
-// shell.exec('skafold')
 skafold().then( message => {
     console.log(message)
   })
