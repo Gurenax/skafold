831b20287b418d765b58812797d064d64f3658e7
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-12-12 23:06:23 +1100

    Refactor route templates and add error handling

--- bin/skafold.js
+++ bin/skafold.js
@@ -1,6 +1,10 @@
 #! /usr/bin/env node
 const { skafold } = require('../index.js')
 console.log('Running Skafold...')
-skafold().then( message => {
+try {
+  skafold().then(message => {
     console.log(message)
   })
+} catch (error) {
+  console.error(`Error: ${error.message}`)
+}
--- index.js
+++ index.js
@@ -1,129 +1,62 @@
 const capitalize = require('lodash/capitalize')
-const _inflection = require("lodash-inflection")
-const { writeToFile } = require('./lib/write-file')
-const mkdirp = require('mkdirp');
+const _inflection = require('lodash-inflection')
+const { writeToFile, checkFileExists } = require('./lib/files')
+const mkdirp = require('mkdirp')
+const {
+  getTemplateImports,
+  getTemplateReadAll,
+  getTemplateReadOne,
+  getTemplatePost,
+  getTemplatePatch,
+  getTemplateDelete,
+  getTemplateExport
+} = require('./lib/templates')
 
 const skafold = () => {
+  // Handle argument check
+  if (process.argv.length <= 2)
+    throw new Error('Please specify the name of the Model')
+
+  // Get function arguments
   const args = [...process.argv]
   const model = args[args.length - 1]
+
+  // Model Name Templates
   const modelLowercased = model.toLowerCase()
   const modelCapitalized = capitalize(model)
   const modelPluralized = _inflection.pluralize(modelLowercased)
-  const outputFolder = 'routes'
-  const outputFile = `${outputFolder}/${modelLowercased}.js`
-  const code = `const express = require('express')
-const ${modelCapitalized} = require('../models/${modelCapitalized}')
 
-const router = express.Router()
+  // Handle model file check
+  try {
+    checkFileExists(`models/${modelCapitalized}.js`)
+  } catch (err) {
+    throw new Error(
+      `Please ensure that the Model file models/${modelCapitalized}.js has already been created`
+    )
+  }
 
-// GET - Read all ${modelPluralized}
-router.get('/${modelPluralized}', (req, res) => {
-  ${modelCapitalized}.find()
-  // Once it has loaded these documents
-  .then(${modelPluralized} => {
-    // Send them back as the response
-    res.json(${modelPluralized})
-  })
-  .catch(error => {
-    res.status(400).json({ error: error.message })
-  })
-})
+  const routesFolder = 'routes'
+  const routesFile = `${routesFolder}/${modelLowercased}.js`
+  const template =
+    getTemplateImports(modelCapitalized) +
+    getTemplateReadAll(modelLowercased, modelCapitalized, modelPluralized) +
+    getTemplateReadOne(modelLowercased, modelCapitalized, modelPluralized) +
+    getTemplatePost(modelLowercased, modelCapitalized, modelPluralized) +
+    getTemplatePatch(modelLowercased, modelCapitalized, modelPluralized) +
+    getTemplateDelete(modelLowercased, modelCapitalized, modelPluralized) +
+    getTemplateExport()
 
-// GET - Read an individual ${modelLowercased} document
-router.get('/${modelPluralized}/:id', (req, res) => {
-  const id = req.params.id
-  // Ask the model for the document with this id
-  ${modelCapitalized}.findById(id)
-    // Once it has loaded this document
-    .then(${modelLowercased} => {
-      // If an ${modelLowercased} was found
-      if(${modelLowercased}) {
-        res.json(${modelLowercased})
-      }
-      // If no ${modelLowercased} was foound
-      else {
-        res.status(404).json({ error: \`${
-          modelCapitalized
-        } not found with id: \${id}\` })
-      }
-    })
-    .catch(error => {
-      // If there was an error, most likely with the format of the id
-      res.status(400).json({ error: error.message })
-    })
-})
-
-// POST - Create a new ${modelLowercased} document
-router.post('/${modelPluralized}', (req, res) => {
-  const attributes = req.body
-  ${modelCapitalized}.create(attributes)
-    .then(${modelLowercased} => {
-      res.status(201).json(${modelLowercased})
-    })
-    .catch(error => {
-      res.status(400).json({ error: error })
-    })
-})
-
-// PATCH - Update a ${modelLowercased} document
-router.patch('/${modelPluralized}/:id', (req, res) => {
-  const id = req.params.id
-  const attributes = req.body
-  ${
-    modelCapitalized
-  }.findByIdAndUpdate(id, attributes, { new: true, runValidators: true })
-    .then(${modelLowercased} => {
-      // If an ${modelLowercased} was found and updated
-      if(${modelLowercased}) {
-        res.status(200).json(${modelLowercased})
-      }
-      // If no ${modelLowercased} was found
-      else {
-        res.status(404).json({ error: \`${
-          modelCapitalized
-        } not found with id: \${id}\` })
-      }
-    })
-    .catch(error => {
-      res.status(400).json({ error: error })
-    })
-})
-
-// DELETE - Destroy a ${modelLowercased} document
-router.delete('/${modelPluralized}/:id', (req, res) => {
-  const id = req.params.id
-  ${modelCapitalized}.findByIdAndRemove(id)
-    .then(${modelLowercased} => {
-      // If an ${modelLowercased} was found and deleted
-      if(${modelLowercased}) {
-        res.status(200).json(${modelLowercased})
-      }
-      // If no ${modelLowercased} was found
-      else {
-        res.status(404).json({ error: \`${
-          modelCapitalized
-        } not found with id: \${id}\` })
-      }
-    })
-    .catch(error => {
-      res.status(400).json({ error: error })
-    })
-})
-
-module.exports = router`
-
-  mkdirp(outputFolder, err => {
-    if(err) {
+  mkdirp(routesFolder, err => {
+    if (err) {
       console.error(err)
       return
     }
   })
 
-  return writeToFile(outputFile, code)
-    .then(()=> {
-      // When the file was completely written
-      return `Skafold: API routes for ${modelCapitalized} were created successfully!`
-    })
+  return writeToFile(routesFile, template).then(() => {
+    // When the file was completely written
+    return `Skafold: API routes for ${modelCapitalized} were created successfully!`
+  })
 }
 
 module.exports = { skafold }
